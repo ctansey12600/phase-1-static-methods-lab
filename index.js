cee5bb6431aa78f5859b98e5f0d78623f38e2fef
caseytansey <ctansey12600@outlook.com>
2021-09-13 17:01:08 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  
+  static sanitize(string){
+    const stringArray = string.split(' ')
+    for(let i = 0; i < stringArray.length; i++){
+      stringArray[i] = stringArray[i].replace(/[^A-Za-z0-9-']+/g, '')
+    }
+    return stringArray.join(' ')
+  }
+
+  static titleize(string){
+    const newString = string.charAt(0).toUpperCase() + string.slice(1)
+    const stringArray = newString.split(' ')
+    const exclude = ['of', 'the', 'a', 'an', 'but', 'and', 'for', 'at', 'by', 'from']
+    for(let i = 1; i < stringArray.length; i++){
+      if(!exclude.includes(stringArray[i])){
+        stringArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1)
+      }
+    }
+    return stringArray.join(' ')
+  }
 }
